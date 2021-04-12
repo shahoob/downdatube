5b38a659ef4ad60028c31eb69733ffc3fbf8c3b3
Shehab Tweel <shehabtweel@gmail.com>
2021-04-12 18:52:15 +0200

    Rewrite

--- README.md
+++ README.md
@@ -2,71 +2,11 @@
 Yet another YouTube downloader.
 
 ## Installation
+Clone this repo (or download a zip file of it)
+
+Then open up ya terminal,
+Go to where you put `downdatube` in,
+Then:
 ```shell
 pip install -r requirements.txt
-```
-
-## Usage
-```
-Usage: main.py [OPTIONS]
-
-Options:
-  -u, --url TEXT
-  -i, --itag INTEGER  The itag for using a stream
-  --help              Show this message and exit.
-```
-
-### Download a video
-```
-(venv) D:\Quick Projects\downdatube>py main.py --url https://youtu.be/Vp3BS7sBzNc
-```
-```
-Going down da tube...
-Seems like that there are 17 streams, It will take a while to fetch all streams. If you have speedy internet, you
-may ignore that. Do you want to wait for the picker or find by itag instead? [y/N]:
-```
-If you say yes:
-```
-
- Which stream?
-
- * video/mp4 | 1.27 MB | 360p | itag: 18
-   video/mp4 | 1.6 MB | 480p | itag: 135
-   video/webm | 1.05 MB | 480p | itag: 244
-   video/mp4 | 745.5 KB | 480p | itag: 397
-   video/mp4 | 830.62 KB | 360p | itag: 134
-   video/webm | 575.57 KB | 360p | itag: 243
-   video/mp4 | 432.46 KB | 360p | itag: 396
-   video/mp4 | 415.17 KB | 240p | itag: 133
-   video/webm | 348.4 KB | 240p | itag: 242
-   video/mp4 | 220.92 KB | 240p | itag: 395
-```
-Navigate up and down and <kdb>Enter</kdb> to select
-If you said no:
-```
-Lemme print all the available streams
-video/mp4 | 1.27 MB | 360p | itag: 18
-video/mp4 | 1.6 MB | 480p | itag: 135
-video/webm | 1.05 MB | 480p | itag: 244
-video/mp4 | 745.5 KB | 480p | itag: 397
-video/mp4 | 830.62 KB | 360p | itag: 134
-video/webm | 575.57 KB | 360p | itag: 243
-video/mp4 | 432.46 KB | 360p | itag: 396
-video/mp4 | 415.17 KB | 240p | itag: 133
-video/webm | 348.4 KB | 240p | itag: 242
-video/mp4 | 220.92 KB | 240p | itag: 395
-video/mp4 | 188.3 KB | 144p | itag: 160
-video/webm | 153.99 KB | 144p | itag: 278
-video/mp4 | 98.83 KB | 144p | itag: 394
-audio/mp4 | 220.62 KB | None | itag: 140
-audio/webm | 100.65 KB | None | itag: 249
-audio/webm | 147.34 KB | None | itag: 250
-audio/webm | 279.35 KB | None | itag: 251
-Which itag?: 
-```
-Then after choosing a stream:
-```
-video/mp4 | 1.6 MB | 480p | itag: 135
-Downloading...
-100%|#######################################################################################|  1.2 MiB/s  1.9 MiB
 ```
--- main.py
+++ main.py
@@ -1,69 +1,32 @@
-# Required stuff
-from pytube import YouTube, Stream
+from typing import Optional
 
-# CLI stuff
-from progressbar import Bar, FileTransferSpeed, UnknownLength, DataTransferBar, Percentage, DataSize
-from pick import pick
-from humanfriendly import format_size
-import click
+import fire
+from pytube import YouTube
+from halo import Halo
 
-@click.command()
-@click.option('-u', '--url', prompt=True, type=str)
-@click.option('-i', '--itag', type=int, help='The itag for using a stream', required=False)
-def main(url: str, itag: int):
-    widgets = [
-        Percentage(),
-        Bar(),
-        FileTransferSpeed(),
-        DataSize()
-    ]
-
-    bar = DataTransferBar(max_value=UnknownLength, widgets=widgets)
-
-    file_size: int
-
-    def updateBar(stream, chunk, remaining):
-        percent = file_size - remaining + 1000000
-
-        try:
-            # updates the progress bar
-            bar.update(round(percent / 1000000, 2))
-        except:
-            # progress bar dont reach 100% so a little trick to make it 100
-            bar.update(round(file_size / 1000000, 2))
-
-    print('Going down da tube...')
-    yt = YouTube(url, on_progress_callback=updateBar)
-
-    def mapStream(stream: Stream):
-        return f'{stream.mime_type} | {format_size(stream.filesize)} | {stream.resolution} | itag: {stream.itag}'
-
-    streams = yt.streams
-
-    s: Stream
-
-    if itag is not None:
-        s = streams.get_by_itag(itag=itag)
-    else:
-        if len(streams) >= 17:
-            if click.confirm(f'Seems like that there are {len(streams)} streams, It will take a while to fetch all '
-                             f'streams. If you have speedy internet, you may ignore that. Do you want to wait for the '
-                             f'picker or find by itag instead?'):
-                s = pick(streams, 'Which stream?', options_map_func=mapStream)[0]
-            else:
-                print('Lemme print all the available streams')
-                [print(mapStream(s)) for s in streams]
-                s = streams.get_by_itag(itag=click.prompt('Which itag?', type=int))
+from corefunctions.get_stream import get_stream
+from corefunctions.helpers import map_stream
+from corefunctions.download import download_stream
 
 
-    print(mapStream(s))
+def download(url: str, itag: Optional[int] = None, prefetch: Optional[bool] = False):
+    with Halo(text='Trying to get a tube... (creating YouTube object...)') as h:
+        yt = YouTube(url)
+        if prefetch:
+            h.text = 'Digging through the tube... (prefetch)'
+            yt.prefetch()
 
-    file_size = s.filesize | s.filesize_approx
-    bar.max_value = s.filesize | s.filesize_approx
+    print('Some info about this tube:\n')
 
-    print('Downloading...')
+    print(yt.title)
+    print(yt.description)
 
-    s.download(filename=s.title)
+    print('\n')
+
+    stream = get_stream(yt=yt, itag=itag)
+    print(map_stream(stream))
+
+    download_stream(yt, stream)
 
 if __name__ == '__main__':
-    main()
+    fire.Fire(download, name='downdatube')
