42d5ff963fa87b32972cf1ba1ae1e2258223c938
Shehab Ali Tweel <shehabtweel@gmail.com>
2022-04-30 22:39:14 +0200

    Reset downdatube

--- corefunctions/helpers.py
+++
@@ -1,6 +0,0 @@
-from humanfriendly import format_size
-from pytube import Stream
-
-
-def map_stream(stream: Stream):
-    return f'{stream.title} | itag: {stream.itag} | {stream.resolution} | {stream.mime_type} | {format_size(stream.filesize)} | is_progressive: {stream.is_progressive}'
